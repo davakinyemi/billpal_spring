bdb542cbbf612a7754403cc7c6005d3530b6c435
Dave tDot <dartysavan@gmail.com>
2024-11-03 23:11:58 +0100

    initialize spring-boot project for billpal

---
+++ src/main/java/io/dav/billpal/BillpalApplication.java
@@ -0,0 +1,13 @@
+package io.dav.billpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillpalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dav/billpal/BillpalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dav.billpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
